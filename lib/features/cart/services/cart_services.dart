fcc605bf5945767b6e3286ece07be14250ecd61b
Gabriel Arnell <gabearnell@gmail.com>
2023-11-08 15:39:25 -0500

    Revert "Fixing overlap"
    This reverts commit 5f86259c9bf595093ab152b312e502601201012a.

--- lib/features/cart/services/cart_services.dart
+++ lib/features/cart/services/cart_services.dart
@@ -39,4 +39,67 @@ class CartServices {
       showSnackBar(context, e.toString());
     }
   }
+
+ Future<List<Product>> fetchCartProducts(BuildContext context) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    List<Product> productList = [];
+    try {
+      http.Response res =
+          await http.get(Uri.parse('$SERVER_URI/api/get-cart-products'), headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-auth-token': userProvider.user.token,
+      });
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          for (int i = 0; i < jsonDecode(res.body).length; i++) {
+            productList.add(
+              Product.fromJson(
+                jsonEncode(
+                  jsonDecode(res.body)[i],
+                ),
+              ),
+            );
+          }
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+    return productList;
+  }
+
+  void checkoutCart({
+    required BuildContext context
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+
+    try {
+      http.Response res = await http.post(
+        Uri.parse('$SERVER_URI/api/checkout'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': userProvider.user.token,
+        }
+      );
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          User user =
+              userProvider.user.copyWith(cart: jsonDecode(res.body)['cart']);
+          userProvider.setUserFromModel(user);
+          showSnackBar(context, 'Order processed!');
+          Navigator.pop(context);
+
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
 }
