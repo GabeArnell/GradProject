8f59eef0666975c5ae66a8637d77d4d895e58f7d
Siddhardha Atluri <60296437+Siddu1357@users.noreply.github.com>
2023-11-08 13:22:38 -0500

    Update Postings

--- lib/features/cart/services/cart_services.dart
+++ lib/features/cart/services/cart_services.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:thrift_exchange/models/product.dart';
 import 'package:http/http.dart' as http;
@@ -9,22 +11,19 @@ import 'package:thrift_exchange/models/user.dart';
 import 'package:thrift_exchange/providers/user_provider.dart';
 
 class CartServices {
-  void addToCart({
+  void removeFromCart({
     required BuildContext context,
     required Product product,
   }) async {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
 
     try {
-      http.Response res = await http.post(
-        Uri.parse('$SERVER_URI/api/add-to-cart'),
+      http.Response res = await http.delete(
+        Uri.parse('$SERVER_URI/api/remove-from-cart/${product.id}'),
         headers: {
           'Content-Type': 'application/json; charset=UTF-8',
           'x-auth-token': userProvider.user.token,
         },
-        body: jsonEncode({
-          'id': product.id!,
-        }),
       );
 
       httpErrorHandle(
